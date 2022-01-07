2e5dcb122ae2a9781db240bc807fb7774aa21979
b <l>
2022-01-07 08:15:19 +0530

    corrected file added

--- remote-script.sh
+++ remote-script.sh
@@ -3,4 +3,5 @@ sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo USER=$2 PASS=$3 docker login -u $USER -p $PASS
 sudo IMAGE=$1 docker-compose -f docker-compose.yml up -d
